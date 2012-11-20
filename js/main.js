b25c4b4b98f175b9665309a0f70b5ce4960a22e1
Tim 'mithro' Ansell <mithro@mithis.com>
2012-11-21 09:17:23 +1100

    Remove evil end of line spaces.

--- js/main.js
+++ js/main.js
@@ -8,43 +8,43 @@ var g_resources = [{
     type: "tmx",
     src: "data/outlines/small.tmx"
 }];
- 
+
 var jsApp = {
     /* ---
- 
+
      Initialize the jsApp
- 
+
      --- */
     onload: function() {
- 
+
         // init the video
         if (!me.video.init('jsapp', 640, 480, false, 1.0)) {
             alert("Sorry but your browser does not support html 5 canvas.");
             return;
         }
- 
+
         // initialize the "audio"
         me.audio.init("mp3,ogg");
- 
+
         // set all resources to be loaded
         me.loader.onload = this.loaded.bind(this);
- 
+
         // set all resources to be loaded
         me.loader.preload(g_resources);
- 
+
         // load everything & display a loading screen
         me.state.change(me.state.LOADING);
     },
- 
+
     /* ---
- 
+
      callback when everything is loaded
- 
+
      --- */
     loaded: function() {
         // set the "Play/Ingame" Screen Object
         me.state.set(me.state.PLAY, new PlayScreen());
- 
+
         // start the game
         me.state.change(me.state.PLAY);
     },
@@ -53,10 +53,10 @@ var jsApp = {
   getTilePosition: function(x, y) {
     var pos = [];
     pos.x = Math.floor(x / me.game.currentLevel.collisionLayer.tilewidth);
-    pos.y = Math.floor(y / me.game.currentLevel.collisionLayer.tileheight);  
+    pos.y = Math.floor(y / me.game.currentLevel.collisionLayer.tileheight);
     return pos;
   },
-  
+
   // get tile position in pixels from pixels
   getTilePosPixels: function(x, y) {
     var tilePos = jsApp.getTilePosition(x, y);
@@ -65,7 +65,7 @@ var jsApp = {
     pos.y = tilePos.y * me.game.currentLevel.collisionLayer.tileheight;
     return pos;
   },
-  
+
   // get tile position in pixels from row and col
   getTileCoord: function(x, y) {
     var pos = [];
@@ -73,42 +73,42 @@ var jsApp = {
     pos.y = y * me.game.currentLevel.collisionLayer.tileheight;
     return pos;
   },
-  
+
   isTileSolid: function(x, y) {
-    var tilePos = jsApp.getTilePosition(x, y);    
+    var tilePos = jsApp.getTilePosition(x, y);
     if (me.game.currentLevel.collisionLayer.layerData[~~(tilePos.x)]) {
-      var collisionTile = me.game.currentLevel.collisionLayer.layerData[~~(tilePos.x)][~~(tilePos.y)];            
+      var collisionTile = me.game.currentLevel.collisionLayer.layerData[~~(tilePos.x)][~~(tilePos.y)];
       if (jQuery.type(collisionTile) == "object") {
-        return true;    
+        return true;
       }
       else {
         return false;
       }
-    }    
+    }
   },
 
- 
+
 };
 // jsApp
 /* the in game stuff*/
 var PlayScreen = me.ScreenObject.extend({
- 
+
     onResetEvent: function() {
         // stuff to reset on state change
         me.levelDirector.loadLevel("small");
         me.input.registerMouseEvent('mousedown', me.game.viewport, this.mouseDown.bind(this));
     },
- 
+
     mouseDown: function(evt) {
 /*
 
-;           
-    myTileProperty = myLayer.tileset.getTileProperties(myTile.tileId); 
-    
+;
+    myTileProperty = myLayer.tileset.getTileProperties(myTile.tileId);
+
     // Uses the property declared in the Tiled - Property Name = material  |  Property Value = fire
     if (myTileProperty.material == "fire")
     {
-       // Do the action  
+       // Do the action
     }
 
 It is definitely possible to add entities and change tiles at runtime!
@@ -122,15 +122,15 @@ To change tiles in your map, first get a reference to the map layer: var backgro
     },
 
     /* ---
- 
+
      action to perform when game is finished (state change)
- 
+
      --- */
     onDestroyEvent: function() {
     }
- 
+
 });
- 
+
 //bootstrap :)
 window.onReady(function() {
     jsApp.onload();
