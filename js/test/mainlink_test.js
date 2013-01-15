4fe2a60d1f02b5c114fb4b626de24af97c8e43d3
ferni <rolguys@gma@gmail.com>
2013-01-15 01:05:41 -0300

    Moved test folder to js folder

---
+++ js/test/mainlink_test.js
@@ -0,0 +1,16 @@
+th.onLevelReady(function () {
+    module("mainlink.js");
+
+    test("Item click", function () {
+
+        $(".items #item_weapon").trigger("click");
+        equal(ui.chosen.type, "weapon", "choose weapon");
+        ok($(".items #item_weapon").hasClass("chosen"), "weapon thumbnail has 'chosen' class");
+
+        $(".items #item_engine").trigger("click");
+        equal(ui.chosen.type, "engine", "choose engine");
+        ok($(".items #item_engine").hasClass("chosen"), "engine thumbnail has 'chosen' class");
+        ok(!$(".items #item_weapon").hasClass("chosen"), "now weapon does not longer have 'chosen' class");
+    });
+
+});
