38d82b711c7592e5ac9783353de4f120ab8c59a0
ferni <fertiberti@gmail.com>
2013-01-22 10:33:54 -0300

    All tests start outside of any callback
    - They started on th.onLevelReady() before
    
     This would allow for QUnit.done() to work properly.

---
+++ js/test/test-helpers_test.js
@@ -0,0 +1,19 @@
+
+    module("test_helpers.js");
+    asyncTest("onLevelReady", function () {
+        th.onLevelReady(function () {
+            ok(me.state.isCurrent(me.state.PLAY), "Level is indeed ready");
+            start();
+        });
+    });
+
+    asyncTest("setMouse", function () {
+        th.onLevelReady(function () {
+            th.mouseBegin();
+            th.setMouse(4, 6);
+            equal(utils.getMouse().x, 4, "x");
+            equal(utils.getMouse().y, 6, "y");
+            th.mouseEnd();
+            start();
+        });
+    });
