a347757c8218c90b1f8c81a8ffdfbfd3fd0afe0c
ferni <fertiberti@gmail.com>
2013-03-07 14:45:06 -0300

    Fixed server-side code lint

--- src/routes/ship.js
+++ src/routes/ship.js
@@ -1,24 +1,44 @@
+/*
+-*- coding: utf-8 -*-
+* vim: set ts=4 sw=4 et sts=4 ai:
+* Copyright 2013 MITHIS
+* All rights reserved.
+*/
+
+/*global require, exports*/
 var redis = require('redis');
 
-exports.save = function(req, res){
+/**
+* Saves the ship
+* @param {Object} req The request object.
+* @param {Object} res The response object.
+*/
+exports.save = function(req, res) {
+    'use strict';
     var data = req.body,
         client = redis.createClient();
 
-    client.hset('ships', data.name, data.buildings, function(error, reply){
+    client.hset('ships', data.name, data.buildings, function(error, reply) {
         res.json({error: error, reply: reply});
     });
 };
 
-exports.load = function(req, res){
+/**
+* Loads a ship
+* @param {Object} req The request object.
+* @param {Object} res The response object.
+*/
+exports.load = function(req, res) {
+    'use strict';
     var name = req.body.name,
         client = redis.createClient();
 
-    client.hget('ships', name, function(error, reply){
-        if(!error){
+    client.hget('ships', name, function(error, reply) {
+        if (!error) {
             res.json(reply);
-        }else{
+        }else {
             res.json(null);
         }
     });
-    
 };
+
