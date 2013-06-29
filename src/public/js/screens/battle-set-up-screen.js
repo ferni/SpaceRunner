e081e848528862ec04f0a5f8d0dfcb744c47ae54
ferni <fertiberti@gmail.com>
2013-06-28 22:01:50 -0300

    Adding battle-set-up screen.

---
+++ src/public/js/screens/battle-set-up-screen.js
@@ -0,0 +1,13 @@
+
+ /*
+-*- coding: utf-8 -*-
+* vim: set ts=4 sw=4 et sts=4 ai:
+* Copyright 2013 MITHIS
+* All rights reserved.
+*/
+
+/*global */
+
+ screens.register('battle-set-up', GameScreen.extend({
+
+ }));
