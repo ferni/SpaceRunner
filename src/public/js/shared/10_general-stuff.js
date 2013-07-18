18498305916dc223e24d5cfbea302958c25193fa
ferni <fertiberti@gmail.com>
2013-07-18 14:41:57 -0300

    Put sh.SharedClass into its own file.

--- src/public/js/shared/10_general-stuff.js
+++ src/public/js/shared/10_general-stuff.js
@@ -7,72 +7,13 @@
 
 /*global */
 
-var sh = require('./08_placement-rules'), _ = sh._;
+var sh = require('./09_shared-class'), _ = sh._;
 if(typeof exports !== 'undefined'){
     sh = module.exports = sh;
 }
 
 (function(sh){
-    var packables = [],
-        initializing = false; //for SharedClass
-    /**
-     * JavaScript Inheritance Helper
-     * (the same as in melonJS)
-     * */
-    sh.SharedClass = function(){};
-    sh.SharedClass.extendShared = function(prop) {
-        // _super rename to parent to ease code reading
-        var parent = this.prototype,
-            fnTest = /xyz/.test(function() {xyz;}) ? /\bparent\b/ : /.*/;
-
-        // Instantiate a base class (but only create the instance,
-        // don't run the init constructor)
-        initializing = true;
-        var proto = new this();
-        initializing = false;
-
-        // Copy the properties over onto the new prototype
-        for ( var name in prop) {
-            // Check if we're overwriting an existing function
-            proto[name] = typeof prop[name] == "function"
-                && typeof parent[name] == "function"
-                && fnTest.test(prop[name]) ? (function(name, fn) {
-                return function() {
-                    var tmp = this.parent;
-
-                    // Add a new ._super() method that is the same method
-                    // but on the super-class
-                    this.parent = parent[name];
-
-                    // The method only need to be bound temporarily, so we
-                    // remove it when we're done executing
-                    var ret = fn.apply(this, arguments);
-                    this.parent = tmp;
-
-                    return ret;
-                };
-            })(name, prop[name]) : prop[name];
-        }
-
-        // The dummy class constructor
-        function Class() {
-            if (!initializing && this.init) {
-                this.init.apply(this, arguments);
-            }
-            //return this;
-        }
-        // Populate our constructed prototype object
-        Class.prototype = proto;
-        // Enforce the constructor to be what we expect
-        Class.constructor = Class;
-        // And make this class extendable
-        Class.extendShared = sh.SharedClass.extendShared;//arguments.callee;
-        Class.extend = function(){
-            throw new Error('"extendShared" should be called instead of "extend"' +
-                ' on a shared entity.');
-        };
-        return Class;
-    };
+    var packables = [];
 
     //SHARED ENTITIES
     sh.TestSharedEntity = sh.SharedClass.extendShared({});
@@ -188,6 +129,7 @@ if(typeof exports !== 'undefined'){
         'mechanoid_frigate'
     ];
 
+    //used in testing
     sh.getProperties = function(object) {
         var props = [], p;
         for(p in object ){
