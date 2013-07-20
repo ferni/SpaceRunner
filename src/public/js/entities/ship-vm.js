1dd01070d706efcedda6caf174d38f26655d28da
ferni <fertiberti@gmail.com>
2013-07-20 07:52:25 -0300

    Check that Ship.getVM found view model is the correct one.

--- src/public/js/entities/ship-vm.js
+++ src/public/js/entities/ship-vm.js
@@ -82,7 +82,8 @@ var ShipVM = function(shipModel) {
     //internal function used by getVM
     function getMatch(model, modelArray, vmArray){
         var index = modelArray.indexOf(model);
-        if(index !== null && typeof index !== 'undefined') {
+        if(index !== null && typeof index !== 'undefined' &&
+            vmArray[index].m === model) {
             return vmArray[index];
         }
         throw 'Did not find view model, try calling update first.';
