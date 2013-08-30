a4283c1cf83b1ce421b8a9b788575e7d75b1636b
ferni <fertiberti@gmail.com>
2013-08-30 13:52:02 -0300

    auth.getId returns int instead of String.

--- src/auth.js
+++ src/auth.js
@@ -1,70 +1,111 @@
-
- /*
+/*
 -*- coding: utf-8 -*-
 * vim: set ts=4 sw=4 et sts=4 ai:
 * Copyright 2013 MITHIS
 * All rights reserved.
 */
 
-/*global */
+/*global require, exports*/
 
- var _ = require('underscore')._,
-     model = require('./model'),
-     chat = require('./chat'),
-     currentPlayers = []; //filled with model.Player;
+var _ = require('underscore')._,
+    model = require('./model'),
+    chat = require('./chat'),
+    currentPlayers = []; //filled with model.Player;
 
- exports.getID = function(req) {
-     //check that there's a player
-     var id = req.session.playerID;
-     if (typeof id === 'undefined') {
+/**
+* Gets the id of the player in session.
+* @param {*} req
+* @return {int}
+*/
+exports.getID = function(req) {
+    'use strict';
+    //check that there's a player
+    var id = req.session.playerID;
+    if (id === undefined) {
         throw 'Player not in session';
-     }
-     return id;
- };
+    }
+    return parseInt(id, 10);
+};
 
- exports.playerByID = function(id) {
-     return _.find(currentPlayers, function(p){
-         return p.id == id;
-     });
- };
+/**
+* Gets the player given an id.
+* @param {int} id
+* @return {*}
+*/
+exports.playerByID = function(id) {
+    'use strict';
+    return _.find(currentPlayers, function(p) {
+        return p.id === id;
+    });
+};
 
- exports.getPlayer = function(req) {
-     return exports.playerByID(exports.getID(req));
- };
+/**
+ * Gets the player in session.
+ * @param {*} req
+ * @return {*}
+ */
+exports.getPlayer = function(req) {
+    'use strict';
+    return exports.playerByID(exports.getID(req));
+};
 
- exports.toUniqueName = function(playerName) {
-     var newName = playerName,
-         number = 1;
-     while(_.any(currentPlayers, function(p){
-         return p.name === newName;
-     })) {
-         newName = playerName + number.toString();
-         number++;
-     }
-     return newName;
- };
+/**
+ * Converts the name so it's different than all the others adding a number
+ * at the end.
+ * @param {String} playerName
+ * @return {String}
+ */
+exports.toUniqueName = function(playerName) {
+    'use strict';
+    var newName = playerName,
+        number = 1;
+    function hasNameEqualToNewName(player) {
+        return player.name === newName;
+    }
+    while (_.any(currentPlayers, hasNameEqualToNewName)) {
+        newName = playerName + number.toString();
+        number++;
+    }
+    return newName;
+};
 
- exports.createNewPlayer = function() {
-     var player = new model.Player({
-         id: currentPlayers.length,
-         name: exports.toUniqueName('Player')
-     });
-     chat.log('Player "'+ player.name +'" connected to server.');
-     currentPlayers.push(player);
-     return player;
- };
+/**
+ * Creates a new player and add it to currentPlayers.
+ * @return {model.Player}
+ */
+exports.createNewPlayer = function() {
+    'use strict';
+    var player = new model.Player({
+        id: currentPlayers.length,
+        name: exports.toUniqueName('Player')
+    });
+    chat.log('Player "' + player.name + '" connected to server.');
+    currentPlayers.push(player);
+    return player;
+};
 
- exports.disconnect = function(req) {
+/**
+ * Disconnects the player.
+ * @param {*} req
+ */
+exports.disconnect = function(req) {
+    'use strict';
     var player = exports.getPlayer(req),
         index = currentPlayers.indexOf(player);
-     currentPlayers.splice(index, 1);
-     req.session.playerID = undefined;
-     chat.log('Player "'+ player.name +'" has been disconnected' +
-         ' from the server.');
- };
+    currentPlayers.splice(index, 1);
+    req.session.playerID = undefined;
+    chat.log('Player "' + player.name + '" has been disconnected' +
+        ' from the server.');
+};
 
- exports.isOnline = function(playerID) {
-     return _.any(currentPlayers, function(p){
-         return p.id == playerID;
-     });
- };
+/**
+ * Checks if the player is in currentPlayers.
+ * @param {int} playerID
+ * @return {bool}
+ */
+exports.isOnline = function(playerID) {
+    'use strict';
+    return _.any(currentPlayers, function(p) {
+        return p.id === playerID;
+    });
+};
