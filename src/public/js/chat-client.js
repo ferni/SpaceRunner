023cd110909e6b50758b7a5ca0963b08190793cb
ferni <fertiberti@gmail.com>
2013-09-01 18:38:22 -0300

    Fixed headers.

--- src/public/js/chat-client.js
+++ src/public/js/chat-client.js
@@ -1,3 +1,10 @@
+/*
+-*- coding: utf-8 -*-
+* vim: set ts=4 sw=4 et sts=4 ai:
+* Copyright 2013 MITHIS
+* All rights reserved.
+*/
+
 var chatClient = (function() {
     'use strict';
     var client = {};
--- src/public/js/game-state.js
+++ src/public/js/game-state.js
@@ -1,9 +1,9 @@
 /*
- -*- coding: utf-8 -*-
- * vim: set ts=4 sw=4 et sts=4 ai:
- * Copyright 2013 MITHIS
- * All rights reserved.
- */
+-*- coding: utf-8 -*-
+* vim: set ts=4 sw=4 et sts=4 ai:
+* Copyright 2013 MITHIS
+* All rights reserved.
+*/
 
 var GameState = function(){
     //sh.Player
