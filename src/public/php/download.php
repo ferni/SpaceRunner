46ad8fef55398503e776c0daafeaf7c87ba8fd1e
ferni <fertiberti@gmail.com>
2013-09-04 16:07:37 -0300

    Deleted unused php files.

--- src/public/php/download.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $data    = $_GET['data'];
-    $name    = $_GET['name'];
-    Header ( "Content-Type: application/text" );
-    Header ( "Content-Length: ".strlen($data) );
-    Header ( "Content-Disposition: attachment; filename=".$name );
-    echo $data;
-?>
