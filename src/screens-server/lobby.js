98d52104caddb21d07d1374a929520bd3ff8f25f
ferni <fertiberti@gmail.com>
2013-09-10 13:27:13 -0300

    Pre-built ship for challenge.

--- src/screens-server/lobby.js
+++ src/screens-server/lobby.js
@@ -34,7 +34,38 @@ routes.add('newchallenge', function(req, res, next) {
             ship,
             battle;
         if (player) {
-            ship = new sh.Ship({tmxName: 'mechanoid_cruiser'});
+            ship = new sh.Ship({jsonString: '{"tmxName":"Mechanoid_Cruiser",' +
+                '"buildings":[{"type":"door","x":14,"y":11,"r":true},' +
+                '{"type":"wall","x":14,"y":8,"r":false},' +
+                '{"type":"wall","x":17,"y":8,"r":false},' +
+                '{"type":"door","x":15,"y":8,"r":false},' +
+                '{"type":"wall","x":14,"y":15,"r":false},' +
+                '{"type":"wall","x":17,"y":15,"r":false},' +
+                '{"type":"door","x":15,"y":15,"r":false},' +
+                '{"type":"engine","x":10,"y":5,"r":false},' +
+                '{"type":"engine","x":10,"y":17,"r":false},' +
+                '{"type":"engine","x":11,"y":11,"r":false},' +
+                '{"type":"console","x":11,"y":10,"r":false},' +
+                '{"type":"weapon","x":24,"y":8,"r":false},' +
+                '{"type":"weapon","x":24,"y":14,"r":false},' +
+                '{"type":"weapon","x":20,"y":17,"r":false},' +
+                '{"type":"component","x":10,"y":7,"r":false},' +
+                '{"type":"component","x":10,"y":15,"r":false},' +
+                '{"type":"console","x":12,"y":5,"r":false},' +
+                '{"type":"console","x":12,"y":18,"r":false},' +
+                '{"type":"weapon","x":20,"y":5,"r":false},' +
+                '{"type":"power","x":20,"y":15,"r":false},' +
+                '{"type":"power","x":20,"y":7,"r":false},' +
+                '{"type":"console","x":25,"y":10,"r":false},' +
+                '{"type":"console","x":25,"y":13,"r":false},' +
+                '{"type":"console","x":19,"y":18,"r":false},' +
+                '{"type":"console","x":19,"y":5,"r":false},' +
+                '{"type":"door","x":22,"y":11,"r":true},' +
+                '{"type":"weak_spot","x":15,"y":6,"r":false},' +
+                '{"type":"weak_spot","x":15,"y":16,"r":false},' +
+                '{"type":"weak_spot","x":19,"y":11,"r":false},' +
+                '{"type":"door","x":18,"y":11,"r":true}],' +
+                '"units":[]}'});
             ship.putUnit({owner: player});
             ship.putUnit({owner: player});
             ship.putUnit({owner: player});
