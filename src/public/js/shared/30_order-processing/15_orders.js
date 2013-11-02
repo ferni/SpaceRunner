ddee1186c37c2c92f761d6d651a076e916630e12
ferni <fertiberti@gmail.com>
2013-11-02 12:47:25 -0300

    Added Order class.

---
+++ src/public/js/shared/30_order-processing/15_orders.js
@@ -0,0 +1,55 @@
+/*
+-*- coding: utf-8 -*-
+* vim: set ts=4 sw=4 et sts=4 ai:
+* Copyright 2013 MITHIS
+* All rights reserved.
+*/
+
+/*global require, exports, module*/
+var sh = require('../30_order-processing/10_actions'), _ = sh._;
+if (typeof exports !== 'undefined') {
+    /**
+     * exports from NodeJS
+     * @type {*}
+     */
+    sh = module.exports = sh;
+}
+
+(function() {
+    'use strict';
+    var Order;
+    Order = sh.SharedClass.extendShared({
+        execute: function(time) {
+            return null;
+        },
+        conditionsOK: function() {
+            return true;
+        }
+    });
+
+    sh.orders = {};
+    sh.orders.Move = Order.extendShared({
+        init: function(unit, ship, from, to) {
+            this.unit = unit;
+            this.ship = ship;
+            this.from = from;
+            this.to = to;
+        },
+        execute: function(time) {
+            if (this.conditionsOK()) {
+                return new sh.actions.Move({
+                    time: time,
+                    unitID: this.unit.id,
+                    from: this.from,
+                    to: this.to,
+                    duration: this.unit.getTimeForOneTile()
+                });
+            }
+            return null;
+        },
+        conditionsOK: function() {
+            //there's no unit ahead
+            return this.ship.unitsMap.at(this.to.x, this.to.y) === 0;
+        }
+    });
+}());
