59402a6383a7ec3daae65fadc074583871846241
ferni <fertiberti@gmail.com>
2014-02-21 23:05:26 -0300

    Upgraded knockout to 3.0.0

--- src/public/js/screens/base/game-screen.js
+++ src/public/js/screens/base/game-screen.js
@@ -5,7 +5,7 @@
 * All rights reserved.
 */
 
-/*global me, jsApp, screens, $*/
+/*global me, jsApp, screens, $, ko*/
 
  /**
   * Parent of all the other screens-html.
@@ -31,72 +31,70 @@
   }));
   * @type {*}
   */
- var GameScreen = me.ScreenObject.extend({
-     init: function(name) {
-         'use strict';
-         if (!name) {
-             throw 'The screen should have a name. If overriding init,' +
-                 ' call this.parent(<name>)';
-         }
-         this.isReset = false;
-         this.parent(true);
-         this.name = name;
-     },
-
+var GameScreen = me.ScreenObject.extend({
+    init: function(name) {
+        'use strict';
+        if (!name) {
+            throw 'The screen should have a name. If overriding init,' +
+                ' call this.parent(<name>)';
+        }
+        this.isReset = false;
+        this.parent(true);
+        this.name = name;
+    },
      /**
-      * This function should not be overridden,
-      * implement onReset instead.
-      */
-     onResetEvent: function(settings) {
-         'use strict';
-         this.parent(true);
-         me.video.clearSurface(me.video.getScreenContext(), 'gray');
-
-         this.displayHtml();
-         this.onHtmlLoaded();
-
-         this.onReset(settings);
-
-         this.isReset = true;
-         jsApp.onScreenReset();
-     },
-     onReset: function(settings) {
-         'use strict';
+     * This function should not be overridden,
+     * implement onReset instead.
+     */
+    onResetEvent: function(settings) {
+        'use strict';
+        this.parent(true);
+        me.video.clearSurface(me.video.getScreenContext(), 'gray');
+        this.displayHtml();
+        this.onHtmlLoaded();
+        this.onReset(settings);
+        this.isReset = true;
+        jsApp.onScreenReset();
+    },
+    onReset: function(settings) {
+        'use strict';
+        //abstract
+        return settings;
+    },
+    /**
+     * This function should not be overridden,
+     * implement onDestroy instead.
+     */
+    onDestroyEvent: function() {
+        'use strict';
+        this.onDestroy();
+        this.isReset = false;
+        this.clearHtml();
+    },
+    onDestroy: function() {
+        'use strict';
+        //abstract
+        return 0;
+    },
+    displayHtml: function() {
+        'use strict';
+        if (!screens.storedHtmls[this.name]) {
+            throw 'Could not find preloaded html for ' + this.name;
+        }
+        $('#screensUi').html(screens.storedHtmls[this.name]);
+    },
+    clearHtml: function() {
+        'use strict';
+        var $screensUi = $('#screensUi');
+        ko.cleanNode($screensUi[0]);
+        $screensUi.html('');
+    },
+    /**
+     * For modifying html and making bindings
+     */
+    onHtmlLoaded: function() {
+        'use strict';
          //abstract
-         return settings;
-     },
-     /**
-      * This function should not be overridden,
-      * implement onDestroy instead.
-      */
-     onDestroyEvent: function() {
-         'use strict';
-         this.onDestroy();
-         this.isReset = false;
-         this.clearHtml();
-     },
-     onDestroy: function() {
-         'use strict';
-         //abstract
-         return 0;
-     },
-     displayHtml: function() {
-         'use strict';
-         if (!screens.storedHtmls[this.name]) {
-             throw 'Could not find preloaded html for ' + this.name;
-         }
-         $('#screensUi').html(screens.storedHtmls[this.name]);
-     },
-     clearHtml: function() {
-         'use strict';
-         $('#screensUi').html('');
-     },
-     /**
-      * For modifying html and making bindings
-      */
-     onHtmlLoaded: function() {
-         'use strict';
-         //abstract
-         return 0;
-     }
- });
+        return 0;
+    }
+});
