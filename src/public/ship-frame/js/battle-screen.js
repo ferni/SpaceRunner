633f11175cd28875f8378fa26d938f96903661f7
ferni <fertiberti@gmail.com>
2014-09-09 16:35:54 -0300

    Created ship-frame folder with some files.

---
+++ src/public/ship-frame/js/battle-screen.js
@@ -0,0 +1,399 @@
+/*
+-*- coding: utf-8 -*-
+* vim: set ts=4 sw=4 et sts=4 ai:
+* Copyright 2013 MITHIS
+* All rights reserved.
+*/
+
+/*global me, screens, ConnectedScreen, gs, sh, ShipVM, ScriptPrediction,
+ScriptPlayer, $, utils, _, draw, ui, make, TILE_SIZE, HALF_TILE, ko, Timeline*/
+
+screens.register('battle', ConnectedScreen.extend({
+    currentTurnID: null,
+    scriptPlayer: null,
+    scriptServer: [],
+    mouseDownPos: null,
+    /**
+     * Gets executed before onReset.
+     */
+    onHtmlLoaded: function() {
+        'use strict';
+        var screen = this;
+        this.readyButton = (function() {
+            var btn = {},
+                $ready = $('#ready-button');
+            btn.enabled = true;
+            $ready.click(function() {
+                if (btn.enabled) {
+                    screen.onReady();
+                }
+            });
+            btn.enable = function() {
+                btn.enabled = true;
+                $ready.removeClass('disabled')
+                    .html('Ready');
+            };
+            btn.disable = function() {
+                btn.enabled = false;
+                $ready.addClass('disabled')
+                    .html('Awaiting players...');
+            };
+            return btn;
+        }());
+
+        if (this.isReset) {
+            this.onResetAndLoaded();
+        }
+        this.htmlLoaded = true;
+    },
+    /**
+     *
+     * @param battle sh.Battle
+     * @param orders Object
+     */
+    onReset: function(battle, orders) {
+        'use strict';
+        this.parent({id: battle.id});
+        this.turnDuration = battle.turnDuration;
+        gs.battle = battle;
+        gs.ship = battle.ships[0];
+        this.stopFetching();
+        console.log('Battle id is ' + this.id);
+        this.shipVM = new ShipVM(gs.ship);
+        this.shipVM.showInScreen();
+        this.shipVM.update();
+        this.scriptPlayer = new ScriptPlayer(this);
+        this.timeline = new Timeline(this);
+        me.input.bindKey(me.input.KEY.ESC, 'escape');
+        me.input.bindKey(me.input.KEY.D, 'delete');
+        me.input.registerMouseEvent('mouseup', me.game.viewport,
+            this.mouseUp.bind(this));
+        me.input.registerMouseEvent('mousedown', me.game.viewport,
+            this.mouseDown.bind(this));
+        me.input.registerMouseEvent('mousemove', me.game.viewport,
+            this.mouseMove.bind(this));
+
+        this.pause();
+
+        if (orders) {
+            battle.insertOrders(orders);
+        }
+        //orders shown for each unit when moving the mouse around
+        this.previewOrders = {};
+        this.prevMouse = {x: 0, y: 0};
+        if (this.htmlLoaded) {
+            this.onResetAndLoaded();
+        }
+    },
+    onDestroy: function() {
+        'use strict';
+        me.input.unbindKey(me.input.KEY.ESC);
+        me.input.unbindKey(me.input.KEY.D);
+        me.input.releaseMouseEvent('mouseup', me.game.viewport);
+        me.input.releaseMouseEvent('mousedown', me.game.viewport);
+        me.input.releaseMouseEvent('mousemove', me.game.viewport);
+    },
+    onResetAndLoaded: function() {
+        'use strict';
+        var screen = this;
+        //Knockout bindings
+        function ViewModel() {
+            this.shipVM = function() {
+                return screen.shipVM;
+            };
+            this.enemyHP = ko.observable(gs.battle.ships[1].hp);
+            this.selectedUnit = ko.observable(null);
+            this.timeline = screen.timeline;
+        }
+        this.htmlVM = new ViewModel();
+        ko.applyBindings(this.htmlVM, document.getElementById('screensUi'));
+        $('#time-line').jScrollPane();
+    },
+    onData: function(data) {
+        'use strict';
+        var screen = this;
+        this.currentTurnID = data.currentTurnID;
+        $('#turn-number').html(this.currentTurnID);
+        if (this.paused && data.scriptReady) {
+            $.post('/battle/getscript', {id: screen.id}, function(data) {
+                var script = new sh.Script().fromJson(data.script);
+                screen.scriptServer = script;
+                screen.scriptPlayer.loadScript(script);
+                screen.shipVM.update();
+                screen.resultingModel = data.resultingModel;
+                screen.resume();
+                screen.stopFetching();
+                $.post('/battle/scriptreceived', {id: screen.id}, function() {
+                    //(informs the server that the script has been received)
+                }).fail(function() {
+                    console.error('Error pinging server.');
+                });
+            });
+        }
+    },
+    update: function() {
+        'use strict';
+        this.parent();
+        if (!this.paused) {
+            var elapsed = me.timer.getTime() - this.turnBeginTime;
+            this.elapsed = elapsed;
+            this.shipVM.update();
+            this.scriptPlayer.update(elapsed);
+            this.htmlVM.enemyHP(gs.battle.ships[1].hp);
+            //update counter
+            $('#elapsed').html(elapsed);
+            if (elapsed >= this.turnDuration) {
+                this.pause();
+            }
+        } else {
+            if (me.input.isKeyPressed('delete')) {
+                _.chain(gs.selected)
+                    .where({name: 'order'})
+                    .each(function(orderVM) {
+                        orderVM.deselect();
+                        orderVM.remove();
+                    });
+            }
+            if (me.input.isKeyPressed('escape')) {
+                _.invoke(gs.selected, 'deselect');
+                this.previewOrders = {};
+            }
+        }
+    },
+    draw: function(ctx) {
+        'use strict';
+        this.parent(ctx);
+        if (this.paused) {
+            if (this.dragBox) {
+                this.dragBox.draw(ctx);
+                utils.setCursor('crosshair');
+            }
+            _.invoke(this.previewOrders, 'draw', ctx);
+        }
+    },
+    mouseDown: function(e) {
+        'use strict';
+        var which = e.which - 1; //workaround for melonJS mismatch
+        if (!this.paused) {
+            return;
+        }
+        if (which === me.input.mouse.LEFT && !this.dragBox) {
+            this.mouseDownPos = utils.getMousePx();
+        }
+
+    },
+    mouseUp: function(e) {
+        'use strict';
+        var mouse = utils.getMouse(),
+            which = e.which - 1, //workaround for melonJS mismatch
+            draggedOriginalPos,
+            self = this;
+        if (!this.paused) {
+            return;
+        }
+        if (which === me.input.mouse.LEFT) {
+            if (this.dragBox) {
+                this.releaseDragBox();
+            } else if (this.dragging) {//an order
+                if (!sh.v.equal(this.dragging.m.destination, mouse)) {
+                    draggedOriginalPos = this.dragging.m.destination;
+                    this.dragging.m.destination = {x: mouse.x, y: mouse.y};
+                    if (this.dragging.m.isValid(gs.ship, gs.player.id)) {
+                        this.dragging.unitVM.orders.valueHasMutated();
+                    } else {
+                        this.dragging.m.destination = draggedOriginalPos;
+                    }
+                    this.dragging.updatePos();
+                }
+                this.dragging = null;
+            } else {
+                _.each(this.previewOrders, function(orderVM, unitID) {
+                    var unit = self.shipVM.getUnitVMByID(unitID);
+                    unit.insertOrder(orderVM.m);
+                });
+            }
+            this.previewOrders = {};
+            this.mouseDownPos = null;
+        }
+    },
+    mouseMove: function() {
+        'use strict';
+        var mouse = utils.getMouse(),
+            mousePx = utils.lastMousePx;
+        if (this.dragging) {
+            this.dragging.setX(mouse.x).setY(mouse.y);
+            return;
+        }
+        if (this.dragBox) {
+            this.dragBox.updateFromMouse(mousePx);
+        } else if (this.mouseDownPos &&
+                (this.mouseDownPos.x - mousePx.x > 5 ||
+                mousePx.x - this.mouseDownPos.x > 5 ||
+                this.mouseDownPos.y - mousePx.y > 5 ||
+                mousePx.y - this.mouseDownPos.y > 5)) {
+            //mouse exceeded 5 pixel threshold, start drag box.
+            this.startDragBox(this.mouseDownPos);
+            this.dragBox.updateFromMouse(mousePx);
+        } else {
+            if (sh.v.equal(mouse, this.prevMouse)) {
+                return;
+            }
+            this.updatePreviewOrders(mouse);
+        }
+        this.prevMouse = mouse;
+    },
+    updatePreviewOrders: function(mouse) {
+        'use strict';
+        var unitsToGiveOrders,
+            self = this;
+        this.previewOrders = {};
+        if (!_.any(gs.ship.getPlayerUnits(gs.player.id),
+                function(u) {
+                    return sh.v.equal(u, mouse);//no ally at mouse pos
+                })) {
+            unitsToGiveOrders = _.filter(this.shipVM.selected(),
+                function(u) {
+                    //can't place order in same spot as another order
+                    return !_.any(u.orderVMs, function(o) {
+                        return sh.v.equal(o.getMarkerTile(), mouse);
+                    }) &&
+                        (u.orders().length === 0 ||
+                            _.last(u.orderVMs).selected());
+                });
+            _.each(unitsToGiveOrders, function(u) {
+                var order = gs.ship.getValidOrderForPos(u.m, mouse);
+                if (order) {
+                    self.previewOrders[u.m.id] = make.vm(order);
+                    self.previewOrders[u.m.id].convertToPreview();
+                }
+            });
+        }
+    },
+    getModelDifferenceUrl: function(aJsonString, bJsonString) {
+        'use strict';
+        var hashObject = {
+                d: {
+                    a: aJsonString,
+                    b: bJsonString
+                }
+            };
+        return 'http://tlrobinson.net/projects/javascript-fun/jsondiff/#' +
+            encodeURIComponent(JSON.stringify(hashObject));
+    },
+    compareModelWithServer: function() {
+        'use strict';
+        var clientString = JSON.stringify(gs.battle.toJson()),
+            serverString = JSON.stringify(this.resultingModel);
+        if (clientString === serverString) {
+            console.log('Client battle model correctly matches the server' +
+                ' battle model.');
+        } else {
+            console.error('Client battle model is different than the server' +
+                ' battle model (left: client, right: server): ' +
+                this.getModelDifferenceUrl(clientString, serverString));
+        }
+    },
+    pause: function() {
+        'use strict';
+        $('#paused-indicator, #ready-button').show();
+        $('#elapsed').hide();
+        this.readyButton.enable();
+        this.scriptPlayer.onPause();
+        this.shipVM.update();
+        if (this.resultingModel) {
+            this.compareModelWithServer();
+        }
+        this.timeline.update();
+        me.game.sort();
+        me.game.repaint();
+
+        _.each(me.game.getEntityByName('order'), function(oVM) {
+            if (oVM.m.isValid(gs.ship, gs.player.id)) {
+                oVM.updatePos();
+            } else {
+                oVM.remove();
+            }
+        });
+        this.elapsed = 0;
+        console.log('--- TURN ' + this.currentTurnID + ' ---');
+        this.paused = true;
+    },
+    resume: function() {
+        'use strict';
+        $('#paused-indicator, #ready-button').hide();
+        $('#elapsed').show();
+        //reset time
+        this.turnBeginTime = me.timer.getTime();
+        _.invoke(this.shipVM.unitVMs, 'deselect');
+        this.paused = false;
+    },
+    //When a player clicks "Ready"
+    onReady: function() {
+        'use strict';
+        var screen = this;
+        screen.readyButton.disable();
+        //send the orders to the server
+        $.post('/battle/ready',
+            {id: this.id}, function(data) {
+                if (data.wasReady) {
+                    console.warn('According to the server, the player ' +
+                        'was already ready.');
+                }
+                screen.startFetching();
+            }, 'json')
+            .fail(function() {
+                console.error('Could not ready player: server error.');
+                screen.readyButton.enable();
+            });
+    },
+    deselectUnits: function() {
+        'use strict';
+        _.chain(gs.selected)
+            .where({name: 'unit'})
+            .invoke('deselect');
+    },
+    updateUnitHud: function() {
+        'use strict';
+        var selected = _.where(gs.selected, {name: 'unit'});
+        if (selected.length === 1) {
+            if (this.htmlVM.selectedUnit() !== selected[0]) {
+                this.htmlVM.selectedUnit(selected[0]);
+            }
+        } else {
+            this.htmlVM.selectedUnit(null);
+        }
+    },
+    startDragBox: function(pos) {
+        'use strict';
+        this.dragBox = new ui.DragBox(pos);
+    },
+    releaseDragBox: function() {
+        'use strict';
+        var self = this;
+        if (this.dragBox) {
+            this.deselectUnits();
+            _.each(this.shipVM.unitVMs, function(u) {
+                var pos, unitRect;
+                if (u.isMine()) {
+                    pos = new me.Vector2d(
+                        u.m.x * TILE_SIZE,
+                        u.m.y * TILE_SIZE
+                    );
+                    unitRect = new me.Rect(pos, TILE_SIZE, TILE_SIZE);
+                    if (self.dragBox.overlaps(unitRect)) {
+                        u.select();
+                    }
+                }
+            });
+            this.dragBox = null;
+        } else {
+            console.warn('Tried to release dragBox but it was already' +
+                ' released.');
+        }
+        utils.setCursor('default');
+    },
+    at: function(x, y) {
+        'use strict';
+        return gs.ship.at(x, y);
+    }
+}));
---
+++ src/public/ship-frame/js/main.js
@@ -0,0 +1,356 @@
+/*
+-*- coding: utf-8 -*-
+* vim: set ts=4 sw=4 et sts=4 ai:
+* Copyright 2013 MITHIS
+* All rights reserved.
+*/
+
+/*global me, _, utils, $, items, RedColorEntity, hullMap,
+html, ShipSelectScreen, ShipBuildingScreen, BattleScreen,
+sh, hullMaps, hullMapGenerator, GameState, gs, chatClient, server, screens*/
+
+//sugar
+var TILE_SIZE = 32 / sh.GRID_SUB, HALF_TILE = 16 / sh.GRID_SUB;
+
+// game resources
+// in the case of the items, set their image name equal to their type.
+var g_resources = [{
+    name: 'outline',
+    type: 'image',
+    src: 'data/img/render/outline.png'
+}, {
+    name: 'selector',
+    type: 'image',
+    src: 'data/img/render/selector.png'
+}, {
+    name: 'pause-icon',
+    type: 'image',
+    src: 'data/img/render/pause-icon.png'
+}, {
+    name: 'weapon',
+    type: 'image',
+    src: 'data/img/render/weapon_01.png'
+}, {
+    name: 'engine',
+    type: 'image',
+    src: 'data/img/render/engine_01.png'
+}, {
+    name: 'power',
+    type: 'image',
+    src: 'data/img/render/power_01.png'
+}, {
+    name: 'console',
+    type: 'image',
+    src: 'data/img/render/console_02.png'
+}, {
+    name: 'component',
+    type: 'image',
+    src: 'data/img/render/components_01.png'
+}, {
+    name: 'door',
+    type: 'image',
+    src: 'data/img/render/door_01.png'
+}, {
+    name: 'wall',
+    type: 'image',
+    src: 'data/img/render/wall_001.png'
+}, {
+    name: 'weakspot',
+    type: 'image',
+    src: 'data/img/render/weakspot.png'
+}, {
+    name: 'metatiles32x32',
+    type: 'image',
+    src: 'data/img/render/metatiles32x32.png'
+}, {
+    name: 'area_01',
+    type: 'tmx',
+    src: 'data/outlines/small.tmx'
+}, {
+    name: 'test',
+    type: 'tmx',
+    src: 'data/outlines/test.tmx'
+}, {
+    name: 'cyborg_frigate',
+    type: 'tmx',
+    src: 'data/outlines/cyborg_frigate.tmx'
+}, {
+    name: 'cyborg_frigate_img',
+    type: 'image',
+    src: 'data/img/render/ships/cyborg/cyborg_frigate_img.png'
+}, {
+    name: 'cyborg_cruiser',
+    type: 'tmx',
+    src: 'data/outlines/cyborg_cruiser.tmx'
+}, {
+    name: 'cyborg_cruiser_img',
+    type: 'image',
+    src: 'data/img/render/ships/cyborg/cyborg_cruiser_img.png'
+}, {
+    name: 'cyborg_battleship1',
+    type: 'tmx',
+    src: 'data/outlines/cyborg_battleship1.tmx'
+}, {
+    name: 'cyborg_battleship1_img',
+    type: 'image',
+    src: 'data/img/render/ships/cyborg/cyborg_battleship1_img.png'
+}, {
+    name: 'cyborg_battleship2',
+    type: 'tmx',
+    src: 'data/outlines/cyborg_battleship2.tmx'
+}, {
+    name: 'cyborg_battleship2_img',
+    type: 'image',
+    src: 'data/img/render/ships/cyborg/cyborg_battleship2_img.png'
+}, {
+    name: 'cyborg_drone',
+    type: 'tmx',
+    src: 'data/outlines/cyborg_drone.tmx'
+}, {
+    name: 'cyborg_drone_img',
+    type: 'image',
+    src: 'data/img/render/ships/cyborg/cyborg_drone_img.png'
+}, {
+    name: 'humanoid_frigate',
+    type: 'tmx',
+    src: 'data/outlines/humanoid_frigate.tmx'
+}, {
+    name: 'humanoid_frigate_img',
+    type: 'image',
+    src: 'data/img/render/ships/humanoid/humanoid_frigate_img.png'
+}, {
+    name: 'humanoid_cruiser',
+    type: 'tmx',
+    src: 'data/outlines/humanoid_cruiser.tmx'
+}, {
+    name: 'humanoid_cruiser_img',
+    type: 'image',
+    src: 'data/img/render/ships/humanoid/humanoid_cruiser_img.png'
+}, {
+    name: 'humanoid_battleship',
+    type: 'tmx',
+    src: 'data/outlines/humanoid_battleship.tmx'
+}, {
+    name: 'humanoid_battleship_img',
+    type: 'image',
+    src: 'data/img/render/ships/humanoid/humanoid_battleship_img.png'
+}, {
+    name: 'humanoid_drone',
+    type: 'tmx',
+    src: 'data/outlines/humanoid_drone.tmx'
+}, {
+    name: 'humanoid_drone_img',
+    type: 'image',
+    src: 'data/img/render/ships/humanoid/humanoid_drone_img.png'
+}, {
+    name: 'liquid_frigate',
+    type: 'tmx',
+    src: 'data/outlines/liquid_frigate.tmx'
+}, {
+    name: 'liquid_frigate_img',
+    type: 'image',
+    src: 'data/img/render/ships/liquid/liquid_frigate_img.png'
+}, {
+    name: 'liquid_cruiser',
+    type: 'tmx',
+    src: 'data/outlines/liquid_cruiser.tmx'
+}, {
+    name: 'liquid_cruiser_img',
+    type: 'image',
+    src: 'data/img/render/ships/liquid/liquid_cruiser_img.png'
+}, {
+    name: 'liquid_battleship',
+    type: 'tmx',
+    src: 'data/outlines/liquid_battleship.tmx'
+}, {
+    name: 'liquid_battleship_img',
+    type: 'image',
+    src: 'data/img/render/ships/liquid/liquid_battleship_img.png'
+}, {
+    name: 'liquid_drone',
+    type: 'tmx',
+    src: 'data/outlines/liquid_drone.tmx'
+}, {
+    name: 'liquid_drone_img',
+    type: 'image',
+    src: 'data/img/render/ships/liquid/liquid_drone_img.png'
+}, {
+    name: 'mechanoid_frigate',
+    type: 'tmx',
+    src: 'data/outlines/mechanoid_frigate.tmx'
+}, {
+    name: 'mechanoid_frigate_img',
+    type: 'image',
+    src: 'data/img/render/ships/mechanoid/mechanoid_frigate_img.png'
+}, {
+    name: 'mechanoid_cruiser',
+    type: 'tmx',
+    src: 'data/outlines/mechanoid_cruiser.tmx'
+}, {
+    name: 'mechanoid_cruiser_img',
+    type: 'image',
+    src: 'data/img/render/ships/mechanoid/mechanoid_cruiser_img.png'
+}, {
+    name: 'mechanoid_battleship',
+    type: 'tmx',
+    src: 'data/outlines/mechanoid_battleship.tmx'
+}, {
+    name: 'mechanoid_battleship_img',
+    type: 'image',
+    src: 'data/img/render/ships/mechanoid/mechanoid_battleship_img.png'
+}, {
+    name: 'mechanoid_drone',
+    type: 'tmx',
+    src: 'data/outlines/mechanoid_drone.tmx'
+}, {
+    name: 'mechanoid_drone_img',
+    type: 'image',
+    src: 'data/img/render/ships/mechanoid/mechanoid_drone_img.png'
+}, {
+    name: 'button',
+    type: 'image',
+    src: 'data/img/render/button.png'
+}, {
+    name: 'creatures',
+    type: 'image',
+    src: 'data/img/render/creatures.png'
+}, {
+    name: 'creatures_16x16',
+    type: 'image',
+    src: 'data/img/render/creatures_16x16.png'
+}, {
+    name: 'star_hit_white',
+    type: 'image',
+    src: 'data/img/render/star_hit_white.png'
+}, {
+    name: 'nothing',
+    type: 'image',
+    src: 'data/img/render/nothing.png'
+}, {
+    name: 'projectile',
+    type: 'image',
+    src: 'data/img/render/projectile.png'
+}, {
+    name: 'markers',
+    type: 'image',
+    src: 'data/img/render/markers.png'
+}, {
+    name: 'charging-weapon-icon',
+    type: 'image',
+    src: 'data/img/render/charging-weapon-icon.png'
+}];
+
+var prebuilt = {
+    humanoid: '{"tmxName":"Humanoid_Cruiser","buildings":[' +
+        '{"type":"Power","x":15,"y":11,"r":false},' +
+        '{"type":"Engine","x":11,"y":9,"r":false},' +
+        '{"type":"Engine","x":11,"y":13,"r":false},' +
+        '{"type":"Weapon","x":22,"y":9,"r":false},' +
+        '{"type":"Weapon","x":22,"y":13,"r":false},' +
+        '{"type":"Component","x":19,"y":11,"r":false},' +
+        '{"type":"Console","x":11,"y":11,"r":false},' +
+        '{"type":"Console","x":11,"y":12,"r":false},' +
+        '{"type":"Console","x":21,"y":9,"r":false},' +
+        '{"type":"Console","x":21,"y":14,"r":false}],' +
+        '"units":[]}'
+};
+
+var hullMaps = {},
+    gs,
+    FIRST_SCREEN;
+
+// jsApp
+var jsApp = {
+    loadReady: false,
+    /* ---
+
+    Initialize the jsApp
+
+    --- */
+    onload: function() {
+        'use strict';
+        // init the video
+        if (!me.video.init('jsapp', 1440, 1344)) {
+            alert('Sorry but your browser does not support html 5 canvas.');
+            return;
+        }
+        // initialize the "audio"
+        //        me.audio.init("mp3,ogg");
+        // set all resources to be loaded
+        me.loader.onload = this.loaded.bind(this);
+        // set all resources to be loaded
+        me.loader.preload(g_resources);
+        // load everything & display a loading screen
+        me.state.change(me.state.LOADING);
+    },
+    generateHullMaps: function() {
+        'use strict';
+        var i, tmxTileMap;
+        window.hullMaps = {};
+        for (i = 0; i < sh.mapNames.length; i++) {
+            tmxTileMap = new me.TMXTileMap(sh.mapNames[i], 0, 0);
+            tmxTileMap.load();
+            hullMaps[sh.mapNames[i]] = hullMapGenerator.get(tmxTileMap);
+        }
+    },
+    /* ---
+    callback when everything is loaded
+    --- */
+    loaded: function() {
+        'use strict';
+        // set screens-html
+        var self = this;
+        window.FIRST_SCREEN = 'lobby';
+        window.gs = new GameState();
+
+        //set development modes
+        if (utils.getParameterByName('auto') === '1') {
+            gs.modes.auto = true;
+        }
+        if (utils.getParameterByName('useprebuilt') === '1') {
+            gs.modes.useprebuilt = true;
+        }
+        this.generateHullMaps();
+        chatClient.start();
+
+        //prepare dom
+        $('#jsapp').bind('contextmenu', function() {
+            return false;//disable context menu
+        }).attr('unselectable', 'on')
+            .css('user-select', 'none')
+            .on('selectstart', false);//disable selection
+        $(window).bind('beforeunload', function() {
+            server.disconnect();
+        });
+
+        screens.loadHtmls(function() {
+            server.init(function(data) {
+                gs.player = new sh.Player(data.player);
+                if (data.battleID !== undefined) {
+                    //player was in a battle, resume it
+                    server.getBattle(data.battleID, function(battleJson) {
+                        me.state.change('battle', new sh.Battle(battleJson),
+                            battleJson.orders);
+                        self.loadReady = true;
+                        self.onAppLoaded();
+                    });
+                } else {
+                    me.state.change(FIRST_SCREEN);
+                    self.loadReady = true;
+                    self.onAppLoaded();
+                }
+            });
+        });
+    },
+    /*
+    useful for testing
+    */
+    onScreenReset: function() {
+        'use strict';
+        return 0;
+    },
+    onAppLoaded: function() {
+        'use strict';
+        return 0;
+    }
+};
