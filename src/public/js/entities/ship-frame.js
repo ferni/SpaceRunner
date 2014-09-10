27050ff34012e5217da9700ccdf8a7acdf339ec2
ferni <fertiberti@gmail.com>
2014-09-10 18:15:58 -0300

    ShipFrame creates and wires up the iframe.

--- src/public/js/entities/ship-frame.js
+++ src/public/js/entities/ship-frame.js
@@ -5,17 +5,26 @@
  * All rights reserved.
  */
 
+/*global HTMLIFrameElement, $ */
+
 var ShipFrame = (function() {
     'use strict';
     /**
      *
-     * @param ship sh.Ship A ship.
+     * @param battle sh.Battle A battle.
+     * @param shipID int The ship ID.
      * @param onEvent Function Callback for emitted events.
      * @constructor
      */
-    function ShipFrame(ship, onEvent) {
-        this.ship = ship;
+    function ShipFrame(battle, shipID, onEvent) {
+        this.battle = battle;
+        this.shipID = shipID;
         this.onEvent = onEvent;
+
+    }
+
+    function sendData(data, iframe) {
+        iframe.contentWindow.postMessage(data, '*');
     }
 
     ShipFrame.prototype = {
@@ -24,9 +33,27 @@ var ShipFrame = (function() {
          */
         init: function(x, y, width, height) {
             //create iframe, pass the model and bind with onEvent
+            var self = this,
+                iframe = $('<iframe href="ship-frame" width="' + width +
+                    '" height="' + height + '"/>')
+                    .css({position: 'absolute', top: y, left: x})[0];
+            $('#screensUi').append(iframe);
+            //listen to messages from the iframe
+            window.addEventListener('message', function(event) {
+                if (event.source === iframe.contentWindow) {
+                    self.onEvent(event.data);
+                }
+            }, false);
+
+            sendData({
+                battleJson: self.battle.toJson(),
+                shipID: self.shipID
+            }, iframe);
+            this.iframe = iframe;
         },
         runScript: function(script) {
             //pass the script to iframe
+            sendData(script.toJson(), this.iframe);
         }
     };
     return ShipFrame;
