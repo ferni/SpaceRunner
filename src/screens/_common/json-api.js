8790b6636d214566a9c9828fafe994c992c6ac57
ferni <fertiberti@gmail.com>
2014-11-30 18:59:14 -0300

    Moved chat api to screens/_common/json-api.js

--- src/screens/_common/json-api.js
+++ src/screens/_common/json-api.js
@@ -4,3 +4,52 @@
 * Copyright 2013 MITHIS
 * All rights reserved.
 */
+
+/*global require, exports, linesInServer*/
+
+var url = require('url'),
+    auth = require('./server-js/auth'),
+    chat = require('./server-js/chat');
+
+
+exports.chat = {
+    /**
+     * Gets all the chat lines.
+     * @param {*} req
+     * @param {*} res
+     */
+    getlines: function(req, res) {
+        'use strict';
+        var queryData = url.parse(req.url, true).query,
+            lastLineId = parseInt(queryData.last, 10),
+            maxLines = queryData.max,
+            lastLineIndex,
+            i,
+            forSending = [];
+        for (i = 0; i < linesInServer.length; i++) {
+            if (linesInServer[i].id === lastLineId) {
+                lastLineIndex = i;
+                break;
+            }
+        }
+        if (maxLines && linesInServer.length - lastLineIndex > maxLines) {
+            lastLineIndex = linesInServer.length - maxLines - 1;
+        }
+        for (i = lastLineIndex + 1; i < linesInServer.length; i++) {
+            forSending.push(linesInServer[i]);
+        }
+        res.json(forSending);
+    },
+    /**
+     * Send a line to the server
+     * @param {*} req
+     * @param {*} res
+     */
+    send: function(req, res) {
+        'use strict';
+        var line = req.body.line;
+        chat.addLine(auth.getPlayer(req).name, line.message);
+        res.json({});
+    }
+};
+
