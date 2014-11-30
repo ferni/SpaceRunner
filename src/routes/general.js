0872d6f73ce7bce9f1c18d8d643cf46ff0baea5e
ferni <fertiberti@gmail.com>
2014-11-30 18:53:43 -0300

    Moved initialization to home screen.

--- src/routes/general.js
+++ src/routes/general.js
@@ -11,40 +11,6 @@ var auth = require('../screens/_common/server-js/auth'),
     sh = require('../shared');
 
 /**
- * Initializes the entire app. Creates the player
- session, or returns the player state if a session is
- already present.
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- */
-exports.init = function(req, res, next) {
-//  TODO: Move this to the on connection callback when socket.io is implemented
-    'use strict';
-    var player;
-    console.log(req.session);
-    if (req.session.playerID === undefined) {
-        //create the player
-        try {
-            player = auth.createNewPlayer();
-            req.session.playerID = player.id;
-        } catch (e) {
-            next(new Error('Could not create new player'));
-        }
-
-    } else {
-        try {
-            player = auth.getPlayer(req);
-        } catch (e2) {
-            next(new Error('Expected player to be logged in'));
-        }
-    }
-    res.render('home', {
-        username: player.name
-    });
-};
-
-/**
  * Checks if the server is online.
  * @param {Object} req
  * @param {Object} res
--- src/screens/home/controller.js
+++ src/screens/home/controller.js
@@ -4,10 +4,40 @@
 * Copyright 2013 MITHIS
 * All rights reserved.
 */
+
+/*global require, module*/
 //HOME
+var auth = require('../_common/server-js/auth');
+
+/**
+ * Initializes the entire app. Creates the player
+ session, or returns the player state if a session is
+ already present.
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
 module.exports = function(req, res, next) {
     'use strict';
+    var player;
+    console.log(req.session);
+    if (req.session.playerID === undefined) {
+        //create the player
+        try {
+            player = auth.createNewPlayer();
+            req.session.playerID = player.id;
+        } catch (e) {
+            next(new Error('Could not create new player'));
+        }
+
+    } else {
+        try {
+            player = auth.getPlayer(req);
+        } catch (e2) {
+            next(new Error('Expected player to be logged in'));
+        }
+    }
     res.render('home/view', {
-        username: 'sucutrule'
+        username: player.name
     });
 };
