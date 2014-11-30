0ce55a8d8c5d8eb372ce1ffebfe8d08d5ec2f472
ferni <fertiberti@gmail.com>
2014-11-30 19:05:33 -0300

    Moved ship api to screens/ship-builder/json-api.js

--- src/screens/ship-builder/json-api.js
+++ src/screens/ship-builder/json-api.js
@@ -4,3 +4,50 @@
 * Copyright 2013 MITHIS
 * All rights reserved.
 */
+
+/*global require, exports, hullMaps*/
+var redis = require('redis');
+
+exports.ship = {
+    /**
+     * Saves the ship
+     * @param {Object} req The request object.
+     * @param {Object} res The response object.
+     */
+    save: function(req, res) {
+        'use strict';
+        var data = req.body,
+            client = redis.createClient();
+
+        client.hset('ships', data.name, data.buildings, function(error, reply) {
+            res.json({error: error, reply: reply});
+        });
+    },
+    /**
+     * Loads a ship
+     * @param {Object} req The request object.
+     * @param {Object} res The response object.
+     */
+    load: function(req, res) {
+        'use strict';
+        var name = req.body.name,
+            client = redis.createClient();
+
+        client.hget('ships', name, function(error, reply) {
+            if (!error) {
+                res.json(reply);
+            } else {
+                res.json(null);
+            }
+        });
+    },
+    /**
+     * Gets the hull maps.
+     * @param {Object} req
+     * @param {Object} res
+     */
+    gethulls: function(req, res) {
+        'use strict';
+        res.json(hullMaps);
+    }
+};
