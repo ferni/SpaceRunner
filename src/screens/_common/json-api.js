9c07fc95206f6d675ad119f73cf144f83d04753a
ferni <fertiberti@gmail.com>
2014-11-30 19:01:49 -0300

    Moved general api to screens/_common/json-api.js

--- src/screens/_common/json-api.js
+++ src/screens/_common/json-api.js
@@ -9,8 +9,8 @@
 
 var url = require('url'),
     auth = require('./server-js/auth'),
-    chat = require('./server-js/chat');
-
+    chat = require('./server-js/chat'),
+    sh = require('./shared-js');
 
 exports.chat = {
     /**
@@ -50,6 +50,30 @@ exports.chat = {
         var line = req.body.line;
         chat.addLine(auth.getPlayer(req).name, line.message);
         res.json({});
+    }
+};
+
+exports.general = {
+    /**
+     * Checks if the server is online.
+     * @param {Object} req
+     * @param {Object} res
+     */
+    ping: function(req, res) {
+        'use strict';
+        res.json({ ok: true });
+    },
+    /**
+     * Gets an array of all the properties of sh , (the namespace for the
+     * shared code between server and client.
+     * This is used to compare the properties of server and client to
+     * ensure that they are the same.
+     * @param {Object} req
+     * @param {Object} res
+     */
+    sharedprops: function(req, res) {
+        'use strict';
+        res.json({properties: sh.getProperties(sh)});
     }
 };
 
