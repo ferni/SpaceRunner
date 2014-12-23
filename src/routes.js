86572c6bbe46472ec4b3b720be5f6a9202ec4f46
ferni <fertiberti@gmail.com>
2014-12-23 20:51:06 -0300

    Added ship-frame to routes.

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,11 @@ exports.register = function(app) {
     'use strict';
     //Screens
     app.get('/', require('./screens/ship-list/controller'));
+    _.each(['ship-builder', 'ship-list', 'battle', 'ship-frame'], function(screen) {
+        app.get('/' + screen, require('./screens/' + screen + '/controller'));
+    });
+
+    //json api
     _.each(['ship-builder', 'ship-list', 'battle'], function(screen) {
         app.get('/' + screen, require('./screens/' + screen + '/controller'));
         _.each(require('./screens/' + screen + '/json-api'),
