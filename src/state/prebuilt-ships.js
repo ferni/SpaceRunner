2b51a228827e554b0168e1be5724dfca85fbad9c
ferni <fertiberti@gmail.com>
2015-01-10 06:59:31 -0300

    Reformatted "prebuilt-ships"

--- src/state/prebuilt-ships.js
+++ src/state/prebuilt-ships.js
@@ -5,49 +5,45 @@
 * All rights reserved.
 */
 
-/*global require, exports*/
+/*global require, module*/
 
 var redis = require('redis'),
     _ = require('underscore')._,
     Ship = require('shared').Ship;
 
-function getAll() {
-    'use strict';
-    var rc = redis.createClient();
-    return rc.lrangeAsync(['hull_ids', 0, -1]).map(function(id) {
-        return rc.hgetAsync(['hull:' + id, 'name']).then(function(reply) {
-            return {
-                id: id,
-                name: reply
-            };
+module.exports = {
+    getAll: function () {
+        'use strict';
+        var rc = redis.createClient();
+        return rc.lrangeAsync(['hull_ids', 0, -1]).map(function (id) {
+            return rc.hgetAsync(['hull:' + id, 'name']).then(function (reply) {
+                return {
+                    id: id,
+                    name: reply
+                };
+            });
         });
-    });
-}
-
-function get(id) {
-    'use strict';
-    var rc = redis.createClient();
-    return rc.hgetallAsync('hull:' + id);
-}
-
-function create(shipType) {
-    'use strict';
-    var rc = redis.createClient(),
-        newShip = new Ship({tmxName: shipType});
-    return rc.incrAsync('next_hull_id').then(function(id) {
-        return rc.hmsetAsync('hull:' + id, {
-            name: shipType,
-            shipJson: JSON.stringify(newShip.toJson())
-        }).then(function() {
-            return id;
+    },
+    get: function (id) {
+        'use strict';
+        var rc = redis.createClient();
+        return rc.hgetallAsync('hull:' + id);
+    },
+    create: function (shipType) {
+        'use strict';
+        var rc = redis.createClient(),
+            newShip = new Ship({tmxName: shipType});
+        return rc.incrAsync('next_hull_id').then(function (id) {
+            return rc.hmsetAsync('hull:' + id, {
+                name: shipType,
+                shipJson: JSON.stringify(newShip.toJson())
+            }).then(function () {
+                return id;
+            });
+        }).then(function (id) {
+            return rc.rpushAsync(['hull_ids', id]).then(function () {
+                return id;
+            });
         });
-    }).then(function(id) {
-        return rc.rpushAsync(['hull_ids', id]).then(function() {
-            return id;
-        });
-    });
-}
-
-exports.getAll = getAll;
-exports.get = get;
-exports.create = create;
+    }
+};
