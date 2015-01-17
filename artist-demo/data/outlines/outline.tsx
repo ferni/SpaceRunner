cba30aee14925b62ed4b3ae409d0203926f159cf
ferni <fertiberti@gmail.com>
2015-01-17 19:31:59 -0300

    Added resource files.

---
+++ artist-demo/data/outlines/outline.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="outline" tilewidth="32" tileheight="32">
+ <image source="../img/render/outline.png" width="192" height="256"/>
+ <tile id="6">
+  <properties>
+   <property name="location" value="back"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="location" value="back"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="location" value="back"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="location" value="back"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="location" value="back"/>
+  </properties>
+ </tile>
+</tileset>
