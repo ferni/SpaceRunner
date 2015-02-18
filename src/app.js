34f244ac47a63e99d8d6f90012a5bb2d78dd13b4
ferni <fertiberti@gmail.com>
2015-02-18 00:09:59 -0300

    Finished upgrading to Express 4.

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.set('view engine', 'handlebars');
 app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
