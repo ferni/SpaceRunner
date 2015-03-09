f4e4f95c3ebee85d8c841fc3e2969b525f05e40e
ferni <fertiberti@gmail.com>
2015-03-09 15:35:36 -0300

    redis-client.js can be configured to use an online store, or local.

--- src/config/redis-client.js
+++ src/config/redis-client.js
@@ -1,12 +1,20 @@
 /*global require, module*/
-var redis = require('redis');
-var Promise = require("bluebird");
-Promise.promisifyAll(redis);
+var redis = require('redis'),
+    Promise = require("bluebird"),
+    useOnline = false,
+    url,
+    redisURL,
+    client;
 
-var url = require('url');
-var redisURL = url.parse('redis://rediscloud:HapP0fPVKcGIVT5d@pub-redis-11175.us-east-1-3.4.ec2.garantiadata.com:11175');
-var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
+Promise.promisifyAll(redis);
+if (useOnline) {
+    url = require('url');
+    redisURL = url.parse('redis://rediscloud:HapP0fPVKcGIVT5d@pub-redis-11175.us-east-1-3.4.ec2.garantiadata.com:11175');
+    client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+    client.auth(redisURL.auth.split(":")[1]);
+} else {
+    client = redis.createClient();
+}
 
 module.exports = client;
 
